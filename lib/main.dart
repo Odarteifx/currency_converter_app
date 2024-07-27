225bd64a3430e9dd32fed4025db8ed04d64ebbee
william lamptey <williamlamp512@gmail.com>
2024-07-27 00:06:25 +0000

    started a currency converter App

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Currency Converter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: (){}, icon: const Icon(Icons.add)),
+        title: const Text('Currency Converter'),
+        actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.edit))],
+      ),
+    );
+  }
+}
